a8d711703521182a3a46ff5f62ec9b293dded61b
Michael Marshall <michaelmarshall220@gmail.com>
2022-08-24 10:15:12 -0500

    added prod env file

--- src/common/utility.ts
+++ src/common/utility.ts
@@ -1,12 +1,10 @@
 export async function fetchAs<REQ, RES>(verb: 'GET' | 'POST', port: number, path: string, data?: REQ) {
-  let host = "https://dev-showcase.etenlab.com";
-
-  if (process.env.REACT_APP_SERVER_URL !== undefined) host = process.env.REACT_APP_SERVER_URL
+  let host = process.env.REACT_APP_SERVER_URL
 
   console.log(`host: ${host}`)
 
   if (verb === 'GET') {
-    const result = await fetch(process.env.REACT_APP_SERVER_URL + `:${port}` + path, {
+    const result = await fetch(host + `:${port}` + path, {
       method: verb,
       headers: {
         'Content-Type': 'application/json;charset=utf-8',
@@ -16,7 +14,7 @@ export async function fetchAs<REQ, RES>(verb: 'GET' | 'POST', port: number, path
     const json = await result.json();
     return json as unknown as RES;
   } else if (verb === 'POST' && data !== undefined) {
-    const result = await fetch(process.env.SERVER_URL + path, {
+    const result = await fetch(host + path, {
       method: verb,
       headers: {
         'Content-Type': 'application/json;charset=utf-8',
