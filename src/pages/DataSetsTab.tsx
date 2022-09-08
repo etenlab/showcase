01594eaedbb883d6696d738a7574b8f623f325f6
Michael Marshall <michaelmarshall220@gmail.com>
2022-09-08 14:16:43 -0500

    working on disqus

--- src/pages/DataSetsTab.tsx
+++ src/pages/DataSetsTab.tsx
@@ -1,6 +1,7 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 // import ExploreContainer from '../components/ExploreContainer';
 import { Iso6392 } from '../components/iso6392';
+import { DiscussionEmbed } from 'disqus-react';
 
 const Tab1: React.FC = () => {
   return (
@@ -11,6 +12,17 @@ const Tab1: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
+        <DiscussionEmbed
+          shortname='example'
+          config={
+            {
+              url: 'dev.etenlab.com',
+              identifier: 'home-1',
+              title: 'Home Page Discussion',
+              language: 'en_US' 
+            }
+          }
+        />
         <Iso6392 />
       </IonContent>
     </IonPage>
