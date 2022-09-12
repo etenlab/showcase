d13e474300a1763f94197c25e95a3e8b8421d110
Michael Marshall <michaelmarshall220@gmail.com>
2022-09-12 08:18:32 -0500

    removed disqus

--- src/pages/DataSetsTab.tsx
+++ src/pages/DataSetsTab.tsx
@@ -1,25 +1,8 @@
-import { IonAccordion, IonAccordionGroup, IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import { Iso6392 } from '../components/iso6392';
-import { CommentCount, DiscussionEmbed } from 'disqus-react';
 import './DataSetsTab.css'
-import { useRef } from 'react';
 
 const Tab1: React.FC = () => {
-
-  const accordionGroup = useRef<null | HTMLIonAccordionGroupElement>(null);
-
-  const toggleAccordion = () => {
-    if (!accordionGroup.current) {
-      return;
-    }
-    const nativeEl = accordionGroup.current;
-
-    if (nativeEl.value === 'dis-1') {
-      nativeEl.value = undefined;
-    } else {
-      nativeEl.value = 'dis-1';
-    }
-  };
 
   return (
     <IonPage>
@@ -30,40 +13,6 @@ const Tab1: React.FC = () => {
       </IonHeader>
       <IonContent fullscreen>
         <Iso6392 />
-
-        <IonButton onClick={toggleAccordion}>
-          <CommentCount
-            shortname='dev-etenlab-com'
-            config={
-              {
-                url: 'https://dev-showcase.etenlab.com',
-                identifier: 'home-1',
-                title: 'Home Page Discussion',
-              }
-            }
-          >
-            {/* Placeholder Text */}
-            Comments
-          </CommentCount>
-        </IonButton>
-
-        <IonAccordionGroup ref={accordionGroup}>
-          <IonAccordion value='dis-1'>
-            <div className='disqus-wrap' slot="content">
-              <DiscussionEmbed
-                shortname='dev-etenlab-com'
-                config={
-                  {
-                    url: 'https://dev-showcase.etenlab.com',
-                    identifier: 'home-1',
-                    title: 'Home Page Discussion',
-                  }
-                }
-              />
-            </div>
-          </IonAccordion>
-        </IonAccordionGroup>
-
       </IonContent>
     </IonPage>
   );
