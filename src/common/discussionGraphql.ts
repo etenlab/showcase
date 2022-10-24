5a755dd0064f048cde9f8cbb05e919afb5685f47
Hiroshi <hiroshi.tan57@gmail.com>
2022-10-24 12:31:56 -0400

    add apollo client for discussion api

---
+++ src/common/discussionGraphql.ts
@@ -0,0 +1,25 @@
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+
+// Instantiate required constructor fields
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'http://localhost:3001/graphql',
+});
+
+export const client = new ApolloClient({
+  // Provide required constructor fields
+  cache: cache,
+  link: link,
+
+  // Provide some optional constructor fields
+  name: 'react-web-client',
+  version: '1.3',
+  queryDeduplication: false,
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'cache-and-network',
+    },
+  },
+});
