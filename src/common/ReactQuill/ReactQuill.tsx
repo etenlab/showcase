8a71dd996d2d02aea3da139d41d6d658d258fd32
Hiroshi <hiroshi.tan57@gmail.com>
2022-11-04 07:22:00 -0400

    modify customized react quill to prevent enter key event

--- src/common/ReactQuill/ReactQuill.tsx
+++ src/common/ReactQuill/ReactQuill.tsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from 'react';
 import "react-quill/dist/quill.snow.css";
 import ReactQuill from "react-quill";
 
@@ -8,8 +9,24 @@ type ReactQuillProps = {
 }
 
 export function CustomReactQuill({value, onKeyUp, onChange}: ReactQuillProps) {
+  const ref = useRef<any>();
+
+  useEffect(() => {
+    if (ref.current) {
+      const editor = ref.current.getEditor();
+      const keyboard = editor.getModule('keyboard');
+      keyboard.bindings[13].unshift({
+        key: 13,
+        handler: () => {
+          return false;
+        }
+      });
+    }
+  }, [])
+
   return (
     <ReactQuill
+      ref={ref}
       theme="snow"
       value={value}
       onKeyUp={onKeyUp}
