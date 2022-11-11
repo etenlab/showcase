21349806b51998b396a8b995e5ff228d11e00442
hiroshi <hiroshi.tan57@gmail.com>
2022-11-11 14:28:59 +0000

    add FileList components to fileUpload

---
+++ src/components/fileUpload/FileUploader/FileUploader.tsx
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import { useDropzone } from "react-dropzone";
+import { FileUploaderContainer } from "./styled";
+
+type FileUploaderProps = {
+  onChangeFile(file: File): void;
+};
+
+export function FileUploader({ onChangeFile }: FileUploaderProps) {
+  const { acceptedFiles, getRootProps, getInputProps } = useDropzone({
+    multiple: false,
+  });
+
+  useEffect(() => {
+    if (acceptedFiles.length > 0) {
+      onChangeFile(acceptedFiles[0]);
+    }
+  }, [acceptedFiles, onChangeFile]);
+
+  return (
+    <>
+      <FileUploaderContainer {...getRootProps()}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      </FileUploaderContainer>
+    </>
+  );
+}
---
+++ src/components/fileUpload/FileUploader/styled.tsx
@@ -0,0 +1,12 @@
+import { styled } from "@mui/material/styles";
+
+export const FileUploaderContainer = styled("div")({
+  display: 'flex',
+  justifyContent: "center",
+  alignContent: "center",
+  border: "2px dashed  #bdacac",
+  borderRadius: '10px',
+  padding: '24px',
+  background: '#f4fbf0',
+  color: '#919191',
+});
