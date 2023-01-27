6ed8901d9343b2767b01d3e36a0ae71fb0450e6c
cwshkut <alexey.shkut@clockwise.software>
2023-01-27 13:07:34 +0000

    add missing client

--- src/common/graphql.ts
+++ src/common/graphql.ts
@@ -28,3 +28,26 @@ export const client = new ApolloClient({
     },
   },
 });
+
+const AGGREGATION_API_URL = process.env.REACT_APP_AGGREGATION_API;
+
+if (!AGGREGATION_API_URL) {
+  throw new Error('AGGREGATION_API_URL is not defined');
+}
+
+export const aggregationClient = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: AGGREGATION_API_URL,
+  }),
+
+  // Provide some optional constructor fields
+  name: 'react-web-client',
+  version: '1.3',
+  queryDeduplication: false,
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'cache-and-network',
+    },
+  },
+});
